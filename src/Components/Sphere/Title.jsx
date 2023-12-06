cc136dfddd7d8d4adbf6a0351eda4f050de21135
wei-chun tseng <wctseng99@gmail.com>
2023-12-06 10:15:36 +0800

    feat: update title section

--- src/Components/Sphere/Title.jsx
+++ src/Components/Sphere/Title.jsx
@@ -28,16 +28,17 @@ function Title({ onTitleClick }) {
         >
           wctseng99@gmail.com
         </a>
-
-        <div className="absolute z-10 lg:left-5 top-10 lg:top-1/2 lg:-translate-y-1/2">
-          <ul className="flex items-center lg:items-start justify-center lg:justify-end lg:flex-col">
+        {/* lg:left-5 top-10 lg:top-1/2 */}
+        <div className="absolute z-10 top-10 lg:top-20 lg:-translate-y-1/2"> 
+        {/* lg:flex-col */}
+          <ul className="flex items-center lg:items-start justify-center lg:justify-end ">
             <li className="mr-5 text-2xl lg:mb-6 lg:ml-10">
               <a
                 className="text-zinc-600 hover:text-zinc-950 flex flex-row items-center group hover:cursor-pointer"
                 onClick={onTitleClick}
               >
-                <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div>
-                <p className="font-mono hidden lg:flex text-base ml-1 group-hover:font-semibold">
+                {/* <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div> */}
+                <p className="font-mono hidden lg:flex text-base ml-1 bg-gradient-to-r from-zinc-700 to-zinc-300 bg-left-bottom group-hover:font-semibold bg-[length:0%_2.0px] bg-no-repeat group-hover:bg-[length:100%_2.0px] transition-all duration-200 ease-out">
                   Portfolio
                 </p>
                 <BsPersonVcardFill className="ml-4" />
@@ -50,8 +51,8 @@ function Title({ onTitleClick }) {
                 target="_blank"
                 rel="noopener"
               >
-                <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div>
-                <p className="font-mono hidden lg:flex text-base ml-1 group-hover:font-semibold">
+                {/* <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div> */}
+                <p className="font-mono hidden lg:flex text-base ml-1 bg-gradient-to-r from-zinc-700 to-zinc-300 bg-left-bottom group-hover:font-semibold bg-[length:0%_2.0px] bg-no-repeat group-hover:bg-[length:100%_2.0px] transition-all duration-200 ease-out">
                   GitHub
                 </p>
                 <BsGithub className="ml-4" />
@@ -64,8 +65,8 @@ function Title({ onTitleClick }) {
                 target="_blank"
                 rel="noopener"
               >
-                <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div>
-                <p className="font-mono hidden lg:flex text-base ml-1 group-hover:font-semibold">
+                {/* <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div> */}
+                <p className="font-mono hidden lg:flex text-base ml-1 bg-gradient-to-r from-zinc-700 to-zinc-300 bg-left-bottom group-hover:font-semibold bg-[length:0%_2.0px] bg-no-repeat group-hover:bg-[length:100%_2.0px] transition-all duration-200 ease-out">
                   LinkedIn
                 </p>
                 <BsLinkedin className="ml-4" />
@@ -78,8 +79,8 @@ function Title({ onTitleClick }) {
                 target="_blank"
                 rel="noopener"
               >
-                <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div>
-                <p className="font-mono hidden lg:flex text-base ml-1 group-hover:font-semibold">
+                {/* <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div> */}
+                <p className="font-mono hidden lg:flex text-base ml-1 bg-gradient-to-r from-zinc-700 to-zinc-300 bg-left-bottom group-hover:font-semibold bg-[length:0%_2.0px] bg-no-repeat group-hover:bg-[length:100%_2.0px] transition-all duration-200 ease-out">
                   Medium
                 </p>
                 <FaMedium className="ml-4" />
