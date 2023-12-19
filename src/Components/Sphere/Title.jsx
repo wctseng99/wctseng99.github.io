6ddcb272354fa427a6dc5eb3b53660304bc8dbe5
wei-chun tseng <wctseng99@gmail.com>
2023-12-19 23:20:46 +0800

    feat: update resume section

--- src/Components/Sphere/Title.jsx
+++ src/Components/Sphere/Title.jsx
@@ -6,21 +6,19 @@ import {
   BsInstagram,
   BsPersonVcardFill,
 } from "react-icons/bs";
-import { IoDocumentText } from "react-icons/io5";
+import { IoFolderOpenSharp } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import { FaMedium } from "react-icons/fa6";
 
 function Title({ onTitleClick }) {
   return (
     <>
-      <div
-        className="flex flex-col items-center justify-center h-screen"
-      >
+      <div className="flex flex-col items-center justify-center h-screen">
         <h1
           className="font-bold text-4xl z-20 text-zinc-950 hover:cursor-pointer hover:text-zinc-500"
           onClick={onTitleClick}
         >
-          w.c. tseng 
+          w.c. tseng
         </h1>
         <a
           className="font-mono mt-1 z-10 font-medium leading-tight tracking-tighter text-zinc-500 hover:text-zinc-950"
@@ -29,10 +27,10 @@ function Title({ onTitleClick }) {
           wctseng99@gmail.com
         </a>
         {/* lg:left-5 top-10 lg:top-1/2 */}
-        <div className="absolute z-10 top-10 lg:top-20 lg:-translate-y-1/2"> 
-        {/* lg:flex-col */}
+        <div className="absolute top-10 lg:top-20 lg:-translate-y-1/2">
+          {/* lg:flex-col */}
           <ul className="flex items-center lg:items-start justify-center lg:justify-end ">
-            <li className="mr-5 text-2xl lg:mb-6 lg:ml-10">
+            <li className="mr-5 text-2xl lg:ml-10">
               <a
                 className="text-zinc-600 hover:text-zinc-950 flex flex-row items-center group hover:cursor-pointer"
                 onClick={onTitleClick}
@@ -41,10 +39,10 @@ function Title({ onTitleClick }) {
                 <p className="font-mono hidden lg:flex text-base ml-1 bg-gradient-to-r from-zinc-700 to-zinc-300 bg-left-bottom group-hover:font-semibold bg-[length:0%_2.0px] bg-no-repeat group-hover:bg-[length:100%_2.0px] transition-all duration-200 ease-out">
                   Portfolio
                 </p>
-                <BsPersonVcardFill className="ml-4 group-hover:-translate-y-1 transition-all" />
+                <IoFolderOpenSharp className="ml-4 group-hover:-translate-y-1 transition-all" />
               </a>
             </li>
-            <li className="mr-5 text-2xl lg:mb-6 lg:ml-10">
+            <li className="mr-5 text-2xl lg:ml-10">
               <a
                 className="text-zinc-600 hover:text-zinc-950 flex flex-row items-center group"
                 href="https://github.com/wctseng99"
@@ -58,7 +56,7 @@ function Title({ onTitleClick }) {
                 <BsGithub className="ml-4 group-hover:-translate-y-1 transition-all" />
               </a>
             </li>
-            <li className="mr-5 text-2xl lg:mb-6 lg:ml-10">
+            <li className="mr-5 text-2xl lg:ml-10">
               <a
                 className="text-zinc-600 hover:text-zinc-950 flex flex-row items-center group"
                 href="https://www.linkedin.com/in/wctseng/"
@@ -72,7 +70,7 @@ function Title({ onTitleClick }) {
                 <BsLinkedin className="ml-4 group-hover:-translate-y-1 transition-all" />
               </a>
             </li>
-            <li className="mr-5 text-2xl lg:mb-6 lg:ml-10">
+            <li className="mr-5 text-2xl lg:ml-10">
               <a
                 className="text-zinc-600 hover:text-zinc-950 flex flex-row items-center group"
                 href="https://medium.com/@wctseng99"
@@ -84,6 +82,20 @@ function Title({ onTitleClick }) {
                   Medium
                 </p>
                 <FaMedium className="ml-4 group-hover:-translate-y-1 transition-all" />
+              </a>
+            </li>
+            <li className="mr-5 text-2xl lg:ml-10">
+              <a
+                className="text-zinc-600 hover:text-zinc-950 flex flex-row items-center group"
+                href="/resume.pdf"
+                target="_blank"
+                rel="noopener"
+              >
+                {/* <div className="hidden lg:flex border border-t-0 group-hover:border-t border-zinc-500 group-hover:border-zinc-600 w-8 group-hover:w-16 transition-all"></div> */}
+                <p className="font-mono hidden lg:flex text-base ml-1 bg-gradient-to-r from-zinc-700 to-zinc-300 bg-left-bottom group-hover:font-semibold bg-[length:0%_2.0px] bg-no-repeat group-hover:bg-[length:100%_2.0px] transition-all duration-200 ease-out">
+                  Resume
+                </p>
+                <BsPersonVcardFill className="ml-4 group-hover:-translate-y-1 transition-all" />
               </a>
             </li>
           </ul>
